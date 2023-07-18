43346b20d62fa95f3ca6e5291acdfd6582faa099
Ujjwal Sharma <113621084+UjjwalSharma01@users.noreply.github.com>
2023-07-18 06:56:54 +0000

    STL start

---
+++ STL/STL.cpp
@@ -0,0 +1,39 @@
+// learn OOPS before this 
+#include <bits/stdc++.h>
+using namespace std;
+// pair example
+void pairexp() {
+    // simple syntax
+    pair<int, int> p= {1,2};
+    cout<<p.first<<" "<<p.second;
+
+}
+int main(){
+    /*
+    STL - standard template library, it is the set of various containers (stores data),algorithms(process data), and iterators (points data just like pointers),
+    pre built in c++ which are used to perfrom certain task which are already being available in the c++ and you don't have to re-invent the wheel
+    
+    things we will be learning here 
+    1. Algorithms
+    2. Functions
+    3. Containers - first will be learning this
+    4. Iterators -  and this
+    
+    */
+
+
+
+   /*
+   PAIRS 
+
+   actually a part of utility library
+   
+   */
+//   pairs
+/* when we are storing array, instead of storing them individually, we can also store them in pairs using pair container
+
+*/
+
+
+    return 0;
+}
