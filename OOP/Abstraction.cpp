df2cfef671ae5e2230b8876c5643f6ced29f83f6
UjjwalSharma01 <ujjwalsh2004@gmail.com>
2023-07-30 23:01:04 +0530

    abstraction completed

---
+++ OOP/Abstraction.cpp
@@ -0,0 +1,83 @@
+/*
+Abstraction -> implementation hiding, don't seperate it from encapsulation, dono ko seperate nahi kr skte 
+they overlap with each other, it overlaps with all 3 pillars
+
+-> encapsulation is a subset of "ABSTRACTION"
+
+if you call them like this (sirf theory main number milege is tarike se)
+Encapsulation - Data hiding
+abstraction -  impelementation hiding
+
+
+DO NOT EXPLAIN THIS IN THE INTERVIEW, IT'S CORRECT BUT NOT FROM A LEARNING PERSPECTIVE, ITS CONFUSING
+
+encapsulation is a subset of abstraction
+
+-> Abstraction can be achieved by access modifiers just like encapsulation
+
+
+
+you can't differentiate between them because jab aapne implementation kre aapne object and class bnai, toh encapsulation toh apne aap achieve ho gayi
+
+there is not coding example to seperate abstraction and encapsulation
+
+
+
+example - sort* a = new qucksort();
+
+ab hamne end user ko srt nam ka pointer pakdta dia usko nahi pta konsi sorting technique use hui h, usko nahi pta konsi implementation use hui h 
+so we can say this is abstraction and, you used the concept of class and  objects toh encapsulation toh vese ho hogayi use
+
+
+confusion hori h toh you can explicity say ki they both everlap each other
+
+
+INTERVIEW SE BOL SKTE H
+
+abstraction -> generalisation (key keyword)
+encapsulation -> parent wrap up
+
+encapsulation - > mere paas ek theli h usme bohot saari cheeze h aur vo theli maine pakad li, so basically its wrapping or encapsulation -> you are wrapping data
+Abstraction - > maine ek sort type ka printer bnaya and maine vo user ko pakda dia ek generic form main, maine nahi btaya ki konsa sort use kr rhe h
+we are basically teying to achieve generalisation by hiding  implementation of various algorithms.
+
+
+we are putting books in a bag - > encapsulation 
+but we are saying this bag contains book, without specifying the books - > abstraction
+
+
+hmare paas, purse h, jiske andar paise h alag alog type ke - encapsulation
+but purse ke andr paise h without telling ki ko kon se notes h is-  abstraction
+ 
+
+ INTERVIEW QUESTION 
+
+ what is the different between compile time and run time polymorphism
+
+ inheritance - diamond problem 
+
+ abstraction and encapsulation - difference - or chalta firta examole  - sorting wala de skte ho
+
+
+
+*/
+
+
+#include <bits\stdc++.h>
+using namespace std;
+int main(){
+
+
+    return 0;
+}
+
+
+/*IMPORTANT POINTS 
+
+
+
+ 
+
+
+*/
+
