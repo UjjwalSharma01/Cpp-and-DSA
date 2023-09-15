d22f9c6e487e98d7a28d7211c60ffec083ad0bb2
Ujjwal Sharma <113621084+UjjwalSharma01@users.noreply.github.com>
2023-09-15 02:19:44 +0000

    final commit of array

--- test.cpp
+++ test.cpp
@@ -1,23 +1,28 @@
+// program to count number of 1s and zeros in a 1D array using the concept of vector
 #include <bits/stdc++.h>
 using namespace std;
-class abc{
-    public:
-    static int x,y;
-    static void print(){
-        cout<<x<<" "<<y<<endl;
-    }
-};
-int abc::x; //definiting that x and y are related to all the instances made via this class
-int abc::y;
 int main(){
-    abc a;
-    abc b;
-    a.x=1;
-    a.y=2;
-    b.x=3;
-    b.y=4;
-    abc::print();
-    a.print();
+    vector <vector<int> > arr; // outer vector ke andr "vector int" type ka data push hoga and inner main int
+    vector <int> a= {1,2,3};
+    vector <int> b= {4,5,6};
+    vector <int> c = {7,8,9};
+
+    arr.push_back(a);
+    arr.push_back(b);
+    arr.push_back(c);
+
+    int size = arr.size(); // row size
+    int sizecol = arr[0].size(); //column size 
+
+    // printing the array vector
+
+    for(int i=0;i<size;i++){
+        for(int j=0;j<sizecol;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    vector<vector<int> > trr(5, vector<int>(5,-8));
 
     return 0;
 }
