cdff6334d039188bfb5cc2d59c879aa5e53bb773
Ujjwal Sharma <113621084+UjjwalSharma01@users.noreply.github.com>
2024-05-10 14:44:55 +0000

    Add recursion level 2 notes and images

--- Recursion/markdown/recursion level2.md
+++ Recursion/markdown/recursion level2.md
@@ -2,8 +2,8 @@
 
 how to find, what to return in basecase?
 look at the function and observe what the functions gives output of, and then you need to return the value of that output in the basecase
-![subsequence](/Recursion/Self%20Notes/image%20copy%207.png.png)
-![subsequence](/Recursion/Self%20Notes/image%20copy%208.png.png)
+![subsequence](/Recursion/Self%20Notes/image%20copy%207.png)
+
 ## Find the min subsequences with the given sum
 ![subsequence](/Recursion/Self%20Notes/image%20copy%204.png)
 __Problem Statement__ -> Given an array of integers, find the minimum number of subsequences with the given sum  
@@ -11,3 +11,32 @@ __approach__ ->
 ![subsequence](/Recursion/Self%20Notes/image%20copy%205.png)
 ![subsequence](/Recursion/Self%20Notes/image%20copy%206.png)
 __solution code__
+
+```cpp
+
+#include <bits/stdc++.h>
+using namespace std;
+int solve (int arr[], int target){
+    if(target == 0) {
+        return 0;
+    }
+    if(target < 0){
+        return INT_MAX;
+    }
+
+    int mini = INT_MAX;
+    for(int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++){
+        int temp = solve(arr, target - arr[i]);
+        if(temp != INT_MAX){
+            mini = min(mini, temp + 1);
+        }
+    }
+    return mini; // minimum number of coins to make this digit
+}
+int main(){
+    int arr[] = {2,3};
+    int target = 12;
+   cout<< solve(arr, target)<<endl;
+    return 0;
+}
+```
--- test.cpp
+++ test.cpp
@@ -29,115 +29,142 @@
 
 
 
-#include <iostream>
+// #include <iostream>
+// using namespace std;
+
+// class Array {
+// private:
+//     int m, n;
+//     int** a;
+//     int** b;
+//     int** c;
+
+// public:
+//     Array(int row, int col) : m(row), n(col) {
+//         a = new int*[m];
+//         b = new int*[m];
+//         c = new int*[m];
+//         for (int i = 0; i < m; i++) {
+//             a[i] = new int[n];
+//             b[i] = new int[n];
+//             c[i] = new int[n];
+//         }
+//     }
+
+//     int getM()  {
+//         return m;
+//     }
+
+//     int getN()  {
+//         return n;
+//     }
+
+//     int** getA()  {
+//         return a;
+//     }
+
+//     int** getB()  {
+//         return b;
+//     }
+
+//     int** getC()  {
+//         return c;
+//     }
+
+//     void setA(int** arr) {
+//         a = arr;
+//     }
+
+//     void setB(int** arr) {
+//         b = arr;
+//     }
+
+//     void setC(int** arr) {
+//         c = arr;
+//     }
+
+//     void inputMatrix(int** matrix, const string& name) {
+//         cout << "INPUT FOR " << name << " MATRIX:" << endl;
+//         for (int i = 0; i < m; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 cout << "Enter the number at position (" << i << ", " << j << "): ";
+//                 cin >> matrix[i][j];
+//             }
+//         }
+//     }
+
+//     void multiplyMatrices() {
+//         for (int i = 0; i < m; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 c[i][j] = 0;
+//                 for (int k = 0; k < n; k++) {
+//                     c[i][j] += a[i][k] * b[k][j];
+//                 }
+//             }
+//         }
+//     }
+
+//     void displayResult() {
+//         cout << "The multiplication of the matrices is:" << endl;
+//         for (int i = 0; i < m; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 cout << "\t" << c[i][j];
+//             }
+//             cout << endl;
+//         }
+//     }
+
+//     ~Array() {
+//         for (int i = 0; i < m; i++) {
+//             delete[] a[i];
+//             delete[] b[i];
+//             delete[] c[i];
+//         }
+//         delete[] a;
+//         delete[] b;
+//         delete[] c;
+//     }
+// };
+
+// int main() {
+//     int row, column;
+//     cout << "Enter the number of rows: ";
+//     cin >> row;
+//     cout << "Enter the number of columns: ";
+//     cin >> column;
+
+//     Array ar(row, column);
+//     ar.inputMatrix(ar.getA(), "FIRST");
+//     ar.inputMatrix(ar.getB(), "SECOND");
+//     ar.multiplyMatrices();
+//     ar.displayResult();
+
+//     return 0;
+// }
+
+
+#include <bits/stdc++.h>
 using namespace std;
+int solve (int arr[], int target){
+    if(target == 0) {
+        return 0;
+    }
+    if(target < 0){
+        return INT_MAX;
+    }
 
-class Array {
-private:
-    int m, n;
-    int** a;
-    int** b;
-    int** c;
-
-public:
-    Array(int row, int col) : m(row), n(col) {
-        a = new int*[m];
-        b = new int*[m];
-        c = new int*[m];
-        for (int i = 0; i < m; i++) {
-            a[i] = new int[n];
-            b[i] = new int[n];
-            c[i] = new int[n];
+    int mini = INT_MAX;
+    for(int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++){
+        int temp = solve(arr, target - arr[i]);
+        if(temp != INT_MAX){
+            mini = min(mini, temp + 1);
         }
     }
-
-    int getM()  {
-        return m;
-    }
-
-    int getN()  {
-        return n;
-    }
-
-    int** getA()  {
-        return a;
-    }
-
-    int** getB()  {
-        return b;
-    }
-
-    int** getC()  {
-        return c;
-    }
-
-    void setA(int** arr) {
-        a = arr;
-    }
-
-    void setB(int** arr) {
-        b = arr;
-    }
-
-    void setC(int** arr) {
-        c = arr;
-    }
-
-    void inputMatrix(int** matrix, const string& name) {
-        cout << "INPUT FOR " << name << " MATRIX:" << endl;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                cout << "Enter the number at position (" << i << ", " << j << "): ";
-                cin >> matrix[i][j];
-            }
-        }
-    }
-
-    void multiplyMatrices() {
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                c[i][j] = 0;
-                for (int k = 0; k < n; k++) {
-                    c[i][j] += a[i][k] * b[k][j];
-                }
-            }
-        }
-    }
-
-    void displayResult() {
-        cout << "The multiplication of the matrices is:" << endl;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                cout << "\t" << c[i][j];
-            }
-            cout << endl;
-        }
-    }
-
-    ~Array() {
-        for (int i = 0; i < m; i++) {
-            delete[] a[i];
-            delete[] b[i];
-            delete[] c[i];
-        }
-        delete[] a;
-        delete[] b;
-        delete[] c;
-    }
-};
-
-int main() {
-    int row, column;
-    cout << "Enter the number of rows: ";
-    cin >> row;
-    cout << "Enter the number of columns: ";
-    cin >> column;
-
-    Array ar(row, column);
-    ar.inputMatrix(ar.getA(), "FIRST");
-    ar.inputMatrix(ar.getB(), "SECOND");
-    ar.multiplyMatrices();
-    ar.displayResult();
-
+    return mini; // minimum number of coins to make this digit
+}
+int main(){
+    int arr[] = {2,3};
+    int target = 12;
+   cout<< solve(arr, target)<<endl;
     return 0;
 }
